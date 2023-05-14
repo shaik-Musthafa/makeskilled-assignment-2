02513db1aafdcc25ec58bbd99ab582cc83d9792e
shaik Musthafa <musthafaskkrnk@gmail.com>
2023-05-14 19:07:33 +0530

    printed palindrome message

--- app.py
+++ app.py
@@ -2,5 +2,12 @@ a='radar'
 b='musthafa'
 
 print(type(a),type(b))
-print(a==a[::-1])
-print(b==b[::-1])
+
+if a==a[::-1]:
+    print(a+ ' is a palindrome' )
+else:
+    print(b+ ' is not a palindrome')
+if b==b[::-1]:
+    print(b+ ' is a palindrome')
+else:
+    print(b+ ' is not a palindrome')   
